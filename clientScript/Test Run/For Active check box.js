b092e886fe491177458ae4f0ad36a73c9a0c0509
varshenilmnas <varsheni.velmurugan@lmnas.com>
2025-06-27 12:46:18 +0530

    Doctypes

---
+++ clientScript/Test Run/For Active check box.js
@@ -0,0 +1,29 @@
+frappe.ui.form.on('Test Run', {
+    is_active_run: function(frm) {
+        if (frm.doc.is_active_run && frm.doc.test_lab) {
+            frappe.call({
+                method: 'frappe.client.get_list',
+                args: {
+                    doctype: 'Test Run',
+                    filters: [
+                        ['test_lab', '=', frm.doc.test_lab],
+                        ['is_active_run', '=', 1],
+                        ['name', '!=', frm.doc.name]  // exclude current doc
+                    ],
+                    fields: ['name']
+                },
+                callback: function(response) {
+                    if (response.message && response.message.length > 0) {
+                        console.log("In")
+                        const conflicting_doc = response.message[0].name;
+                        const test_lab_id = frm.doc.test_lab;
+                        frappe.msgprint(
+                            `For this <b>${test_lab_id}</b>, the Test Run <b>${conflicting_doc}</b> is already active.`
+                        );
+                        frm.set_value('is_active_run', 0);
+                    }
+                }
+            });
+        }
+    }
+});
