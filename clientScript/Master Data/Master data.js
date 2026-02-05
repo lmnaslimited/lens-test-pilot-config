9fd89e2bf381b874ed17848c6e11090cb58b79df
Fathima786Irfana <fathima.irfana@gmail.com>
2026-02-05 18:05:23 +0530

    Removed the field value equation

--- clientScript/Master Data/Master data.js
+++ clientScript/Master Data/Master data.js
@@ -20,10 +20,6 @@ frappe.ui.form.on('Master Data', {
                         frm.set_value('action', ldSource.action);
                     }
 
-                    if (ldSource.test_type) {
-                        frm.set_value('action', ldSource.test_type);
-                    }
-
                     // Copy 'document' field value from the Test Case Configurator
                     if (ldSource.document) {
                         frm.set_value('document', ldSource.document);
